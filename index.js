8d90c1c621d0fab93bcf3c35b73813fd2cef6620
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-01-26 15:53:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+let def = "hello world";
+let myName = "Erjan";
+let myCar = "BMW";
+let mybook ="Aitmatov";
+let myMama = "Syrga";
+let myCountry = "Kyrgyzstan";
+let myVillage ="cholpon";
+let myColor ="blue";
+let myTelephone="Redmi";
+let myFriend ="Saikal";
+
+
+
+let result;
+result= 5;
+result=3+8;
+result=false;
+result="hello"
+
+let myBirthday=25;
+let month = 1;
+let day =24;
+let year=2023;
+let fingers=10;
+
+let example = 10 / 10;
+console.log(example) 
+
+let example1= 25**(1/2);
+console.log(example1)
+  
+
+let day1 =30 / 10;
+console.log(day1)
+
+let card = 100 **(1/2);
+console.log(card)
+
+
+let number = 3;
+if (number>0) {
+  console.log("Число " + number +" положительное");
+}
+
+else if (number <0){
+  console.log("Число " + number +" отрицательное")
+}
+else if (number === 0){
+console.log("Число ноль не  отрицательное и положительное");
+}
+
+else {
+  console.log(number + " не является числом")
+}
+ let num= 5;
+ if (num % 2 === 0){
+console.log(num + "is even");
+ }
+else if(num % 2 ===1){
+console.log (num + "is odd")
+}
+else {
+  console.log(num + "is not number")
+}
+
+
+
+
+//вывести каким сезоном является ли "Январь"?
+let month1= "январь";
+if (month1 === "декабрь" ||  month1 ==="январь" ||  month1 ==="февраль") {
+  console.log("это зима")
+}
+
